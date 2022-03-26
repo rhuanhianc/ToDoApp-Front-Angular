3044f9f859a3a5cf949aeaf5ca1828bfc58f6a71
Rhuan S. Hianc <rhuanhianc123@gmail.com>
2022-03-26 11:08:54 -0300

    Update todo.service.ts

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -16,5 +16,12 @@ export class TodoService {
     salvar(todo :ToDo) : Observable<ToDo> {
       return this.http.post<ToDo>('http://localhost:8080/api/todos', todo)
     }
+    getAll() : Observable<ToDo[]> {
+        return this.http.get<ToDo[]>('http://localhost:8080/api/todos')
+        }
+
+    delete(todo : ToDo) : Observable<ToDo> {
+        return this.http.delete<ToDo>(`http://localhost:8080/api/todos/${todo.id}`)
+        }
    
 }
